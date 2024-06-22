c3568e942e916a3f6e5c7c75cef47b4e1010dcaa
Facundo Caamaño <facundoadriancaamano@outlook.com>
2024-06-21 22:05:28 -0300

    modulos estilos rutas componentes(navbar,main)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'to-do-list',
+    loadChildren: ()=> import('./features/to-do-list/to-do-list.module').then(m => m.ToDoListModule)
+  },
+  // {
+  //   path:'**',
+  //   redirectTo:'home'
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 't.e.u';
+  menuOpen:boolean = false
+  width= window.innerWidth
+  constructor(){
+    console.log(this.width);
+    
+  }
+
+  stateMenu(){
+    this.menuOpen = !this.menuOpen
+    console.log(this.menuOpen);
+    
+  }
 }
