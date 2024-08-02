ecc9b30628c3b5a4feda692fd37991c54d0c7973
Facundo Caamaño <facundoadriancaamano@outlook.com>
2024-08-02 19:02:10 -0300

    key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
       production:true,
-      urlRickAndMOrty:'https://rickandmortyapi.com/api/'
+      urlRickAndMOrty:'https://rickandmortyapi.com/api/',
+
+      nasaKey: import.meta.env['NG_APP_KEY_NASA'],
+      urlNASA:'https://api.nasa.gov/neo/rest/v1/'
 };
